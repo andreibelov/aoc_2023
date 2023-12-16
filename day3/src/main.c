bdd9fd8846da852ea7f4ae60948710db06825f8a
Andrei Belov <andrei.belov@brambles.com>
2023-12-16 16:04:52 +0000

    updated the template

--- day3/src/main.c
+++ day3/src/main.c
@@ -3,8 +3,44 @@
 
 #include "main.h"
 
+int	get_answer(FILE *fp)
+{
+	ssize_t		nread;
+	int			d = 0;
+	size_t		len = 0;
+	char		*line = NULL;
+
+	nread = getline(&line, &len, fp);
+	while (nread != -1)
+	{
+		ft_printf("%s", line);
+		nread = getline(&line, &len, fp);
+	}
+	free(line);
+	return (d);
+}
 int	main(int argc, char **argv)
 {
-	ft_printf("Hey ho!");
+	FILE	*fp;
+	int		argg;
+	int 	result;
+
+	argg = 1;
+	if (argc < 1)
+		return (EX_NOINPUT);
+	if (argc > 1)
+		while (argg < argc) {
+			fp = fopen(argv[argg++], "r");
+			result = get_answer(fp);
+			fclose(fp);
+			ft_printf("Answer is: \"%d\"", result);
+		}
+	else {
+//		read_single_file(ft_fopen("/dev/stdin", "r"));
+		fp = fopen(INPUT_FILE_NAME, "r");
+		result = get_answer(fp);
+		fclose(fp);
+		ft_printf("Answer is: \"%d\"", result);
+	}
 	return (EX_OK);
 }
--- template/src/main.c
+++ template/src/main.c
@@ -3,7 +3,34 @@
 
 #include "main.h"
 
+
+int	get_answer(FILE *fp)
+{
+	return (0);
+}
+
 int	main(int argc, char **argv)
 {
+	FILE	*fp;
+	int		argg;
+	int 	result;
+
+	argg = 1;
+	if (argc < 1)
+		return (EX_NOINPUT);
+	if (argc > 1)
+		while (argg < argc) {
+			fp = fopen(argv[argg++], "r");
+			result = get_answer(fp);
+			fclose(fp);
+			ft_printf("Answer is: \"%d\"", result);
+		}
+	else {
+//		read_single_file(ft_fopen("/dev/stdin", "r"));
+		fp = fopen(INPUT_FILE_NAME, "r");
+		result = get_answer(fp);
+		fclose(fp);
+		ft_printf("Answer is: \"%d\"", result);
+	}
 	return (EX_OK);
 }
